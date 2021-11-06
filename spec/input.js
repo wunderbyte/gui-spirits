5436d0cde92192bc1b40c53d724a33ec161e70e4
Wired Earp <wiredearp@gmail.com>
2021-11-06 09:00:16 +0100

    Tests continue

--- spec/input.js
+++ spec/input.js
@@ -13,19 +13,33 @@ const plugins = new Map([
 	['shadow', ShadowPlugin]
 ]);
 
+const append = (target, elm) => target.appendChild(elm);
+const create = (name) => document.createElement(name);
+const locate = (name) => document.querySelector(name);
+
+beforeEach(() => append(document.body, create('stage')));
+afterEach(() => locate('stage').remove());
+
+/**
+ * Register component and insert it. Returns a function to remove it.
+ * @param {string} tag 
+ * @param {Function} controller 
+ * @returns {Function} cleanup
+ */
 function test(tag, controller) {
 	summon(tag, controller, plugins);
-	document.body.appendChild(document.createElement(tag)).remove();
+	append(locate('stage'), create(tag));
 }
 
 describe('AttPlugin', function () {
-	it('contains spec with an expectation', () => {
+	it('contains spec with an expectation', (done) => {
 		test('plugin-att', ({ att }) => {
 			att.set('string', 'ok').set('number', 23).set('boolean', false);
 			expect(att.get('string')).toBe('ok');
 			expect(att.get('number')).toBe(23);
 			expect(att.get('boolean')).toBe(false);
 			expect(att.has('boolean')).toBe(true);
+			done();
 		});
 	});
 });
